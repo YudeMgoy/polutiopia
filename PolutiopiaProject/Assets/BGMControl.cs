c931b4ddcc0c4dbace08b21ae2afb2d77a5c4ac5
Charlesss7 <90251876+Charlesss7@users.noreply.github.com>
2022-11-28 12:12:56 +0700

    Scene connection and level 1
    Adding level 1 to level select, music pref, lacking sfx, camera, pause menu

---
+++ PolutiopiaProject/Assets/BGMControl.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGMControl : MonoBehaviour
+{
+    public AudioSource music;
+    // Start is called before the first frame update
+    void Start()
+    {
+        music.volume = PlayerPrefs.GetFloat("MusicVolume");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
