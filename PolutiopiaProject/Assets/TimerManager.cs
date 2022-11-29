96ed62779d1f2de55ad75c69017430efa0193fbe
Hamdandi <mhd.hamdandi@gmail.com>
2022-11-29 19:50:19 +0700

    test

---
+++ PolutiopiaProject/Assets/TimerManager.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerManager : MonoBehaviour
+{
+    public LevelManager LevelManager;
+
+    [Header("Timer Setting")]
+    public Image timerImage;
+    public GameObject pausePanel;
+    public int duration;
+    private int remainingDuration;
+    public TimerFill timerFill;
+
+    private void Start()
+    {
+        Being(duration);
+    }
+
+    private void Being(int Seconds)
+    {
+        remainingDuration = Seconds;
+        StartCoroutine(UpdateTimer());
+    }
+
+    IEnumerator UpdateTimer()
+    {
+        while (remainingDuration >= 0)
+        {
+            timerFill.UpdateFill((float)remainingDuration / duration);
+            timerImage.fillAmount = Mathf.InverseLerp(0, duration, remainingDuration);
+            remainingDuration--;
+            yield return new WaitForSeconds(1);
+        }
+        OnEnd();
+        LevelManager.GameOver(true);
+    }
+
+    public void OnEnd()
+    {
+        // LevelManager.GameOver(true);
+        print("times Up");
+    }
+
+    public void PauseGame()
+    {
+        // Time.timeScale = 0;
+        StopAllCoroutines();
+    }
+
+    public void ResumeGame()
+    {
+        // Time.timeScale = 1;
+        StartCoroutine(UpdateTimer());
+        pausePanel.SetActive(true);
+    }
+
+
+}
