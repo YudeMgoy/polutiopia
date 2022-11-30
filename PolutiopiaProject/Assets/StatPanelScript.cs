b9c3684b9ec49bb6a25c87cfc17656445ad87ddb
YudeGinvo <wahyudi@ginvostudio.com>
2022-11-30 18:53:20 +0700

    Fix Bug Harga Jual

--- PolutiopiaProject/Assets/StatPanelScript.cs
+++ PolutiopiaProject/Assets/StatPanelScript.cs
@@ -20,9 +20,9 @@ public class StatPanelScript : MonoBehaviour
         Building _building = LevelManager.Instance.SelectedBuilding;
         nameText.text = _building.name;
         moneyText.text = "Buy Price (Money) : "+_building.MoneyPrice.ToString();
-        sellMoneyText.text = "Sell Price (Money) : "+(_building.MoneyPrice * .5).ToString();
+        sellMoneyText.text = "Sell Price (Money) : "+(_building.moneySellBonus).ToString();
         woodText.text = "Buy Price (Wood) : "+_building.WoodPrice.ToString();
-        sellWoodText.text = "Sell Price (Wood) : "+(_building.WoodPrice * .5).ToString();
+        sellWoodText.text = "Sell Price (Wood) : "+(_building.woodSellBonus).ToString();
         descriptionText.text = _building.description;
         buildingImage.sprite = _building.UIImage;
     }
